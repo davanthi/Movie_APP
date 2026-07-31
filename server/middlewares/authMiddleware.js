601c19b69205d6b2c0257572568b3320e4527445
davanthi <davanthijadi17@gmail.com>
2026-07-31 18:57:40 +0530

    authorization issues

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,26 +1,28 @@
 const jwt = require("jsonwebtoken");
-const User=require('../models/user.model.js')
-const isAuth=async(req,res,next)=>{
- const token = req.cookies.jwtToken;
- if (!token) {
-   return res.status(401).json({
-     success: false,
-     message: "Not authorized, no token",
-   });
- }
- try{
-const decoded=jwt.verify(token,process.env.JWT_SECRET);
-req.userId=decoded.userId;
-  const user = await User.findById(decoded.userId).select("-password");
-  if (user) {
-    req.user = user;
+const User = require("../models/user.model.js");
+const isAuth = async (req, res, next) => {
+  console.log("Cookies:", req.cookies);
+  console.log("Token:", req.cookies.jwtToken);
+  const token = req.cookies.jwtToken;
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Not authorized, no token",
+    });
   }
-next();
- }catch(error){
-  return res.status(401).json({
-    success: false,
-    message: "Not authorized, token validation failed",
-  });
- }
-}
-module.exports=isAuth;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    const user = await User.findById(decoded.userId).select("-password");
+    if (user) {
+      req.user = user;
+    }
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Not authorized, token validation failed",
+    });
+  }
+};
+module.exports = isAuth;
